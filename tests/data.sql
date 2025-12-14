236c59191ba2b69633a432faaf128a13ed07eb48
alejandro_1425 <alejandro_1425@outlook.com>
2025-12-14 21:25:43 +0100

    Delete tests directory

--- tests/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO usuario (username, password) VALUES ('usuario', 'pbkdf2:sha256:260000$bUFpf66ns03ZSUuZ$4d51141049a70e187af7b5b1b07c0e84a88c06f4c64f4c202b02d2c4ff4368fa');
-INSERT INTO tarea (titulo, descripcion, autor_id) VALUES ('Primera tarea', 'Descripción inicial', 1);
